843a75b6cb55aa4a69502ac8a6f6962706e6fc38
IrfanHakimm <mirfanhakim08@gmail.com>
2023-11-14 02:03:29 +0700

    Add task

--- app.js
+++ app.js
@@ -1,64 +1,46 @@
-import express, { json } from "express";
-import { createConnection } from "mysql";
-import { exec } from "child_process";
+const express = require("express");
+const BodyParser = require("body-parser");
+const mysql = require("mysql");
 
 const app = express();
-const port = 3000;
+app.use(BodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.set("views", "views");
 
 // MySQL Configuration
-const mysqlConfig = {
-  host: "your_mysql_host",
-  user: "your_mysql_user",
-  password: "your_mysql_password",
-  database: "your_mysql_database",
-};
-
-// Create MySQL connection
-const connection = createConnection(mysqlConfig);
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "kka",
+});
 
 // Connect to MySQL
-connection.connect((err) => {
+db.connect((err) => {
   if (err) {
     console.error("Error connecting to MySQL:", err);
     return;
   }
   console.log("Connected to MySQL");
-});
-// Handle adding a task
-app.post("/addTask", json(), (req, res) => {
-  const { taskName, subject, deadline, importance, urgency } = req.body;
 
-  // Insert data into MySQL
-  const sql =
-    "INSERT INTO tasks (task_name, subject, deadline, importance, urgency) VALUES (?, ?, ?, ?, ?)";
-  const values = [taskName, subject, deadline, importance, urgency];
+  app.get("/", (req, res) => {
+    res.render("index");
+  });
 
-  connection.query(sql, values, (err, result) => {
-    if (err) {
-      console.error("Error adding task to MySQL:", err);
-      res.status(500).json({ error: "Internal Server Error" });
-      return;
-    }
-    console.log("Task added to MySQL:", result);
-    res.json({ success: true });
+  app.post("/addTask", (req, res) => {
+    const insertTask = `INSERT INTO tasks (taskName, subject, deadline, importance, urgency) VALUES ('${req.body.taskName}', '${req.body.subject}', '${req.body.deadline}', '${req.body.importance}', '${req.body.urgency}');`;
+    db.query(insertTask, (err, result) => {
+      if (err) {
+        console.error("Error executing query:", err);
+        res.status(500).send("Internal Server Error");
+        return;
+      }
+      console.log("Task Inserted successfully");
+      res.redirect("/");
+    });
   });
 });
 
-// Handle generating a graph
-app.post("/generateGraph", (req, res) => {
-  // Trigger the Python script using child_process
-  exec("python generate_graph.py", (error, stdout, stderr) => {
-    if (error) {
-      console.error("Error executing Python script:", error);
-      res.status(500).json({ error: "Internal Server Error" });
-      return;
-    }
-    const graphData = JSON.parse(stdout);
-    console.log("Graph generated successfully:", graphData);
-    res.json({ success: true, graphData });
-  });
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(8000, () => {
+  console.log(`Server is running on http://localhost:${8000}`);
 });
