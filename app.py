a21a011dadd4096293195b8ea9a9abf20dfffe1c
IrfanHakimm <mirfanhakim08@gmail.com>
2023-11-15 20:05:12 +0700

    generate table A*

--- app.py
+++ app.py
@@ -9,34 +9,32 @@ def generateTable():
         database="kka"
     )
 
-    taskNames = []
-    subjects = []
-    dl = []
-    importancies = []
-    urgencies = []
+    list_tasks = {}
 
     temp = db.cursor()
     temp.execute("SELECT * FROM tasks")
 
-    for row in temp:
-        taskNames.append(row[0])
-        subjects.append(row[1])
-        importancies.append(row[3])
-        urgencies.append(row[4])
+    for index, row in enumerate(temp):
+        task_info = {
+            "taskName": row[0],
+            "subject": row[1],
+            "dl": None,
+            "importance": row[3],
+            "urgency": row[4]
+        }
 
         deadline = row[2]
         curr_date = datetime.now().date()
         days_until_deadline = (deadline - curr_date).days
-        dl.append(days_until_deadline)
+        task_info["dl"] = days_until_deadline
+
+        list_tasks[index] = task_info
 
     temp.close()
 
-    return taskNames, subjects, dl, importancies, urgencies
+    return list_tasks
 
-taskNames, subjects, dl, importancies, urgencies = generateTable()
+list_tasks = generateTable()
 
-print("Task Names:", taskNames)
-print("Subjects:", subjects)
-print("Deadlines:", dl)
-print("Importances:", importancies)
-print("Urgencies:", urgencies)
+for key, value in list_tasks.items():
+    print(f"Task {key}: {value}")
